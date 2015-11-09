b27b78d99e77859b629575135e531ccab4d824b4
Dániel Kiss <kissdani89@gmail.com>
2015-11-09 02:37:52 +0100

    util

---
+++ DatagramService/Host/DatagramServiceMexHost.cs
@@ -0,0 +1,58 @@
+﻿using DynamicEndpointUtils;
+using System;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+
+namespace V37ZEN.DatagramService.Host
+{
+    class DatagramServiceMexHost :IDisposable
+    {
+        private ServiceHost host;
+
+        public Uri Address { get; set; }
+
+        public ServiceMetadataBehavior getServiceMetadataBehavior()
+        {
+            return ServiceMetadataBehaviorUtil.getServiceMetadataBehavior(this.Address);
+        }
+
+        public void Open()
+        {
+            this.host = ServiceMetadataBehaviorUtil.DebugServiceHost(typeof(EchoDatagramService),this.Address);
+            this.host.Open();
+        }
+
+        public void Close()
+        {
+            this.host.Close();
+        }
+
+        #region Disposable
+        // Flag: Has Dispose already been called?
+        bool disposed = false;
+
+        public void Dispose()
+        {
+            this.Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected void Dispose(bool disposing)
+        {
+            if (disposed)
+            {
+                return;
+            }
+            if (disposing)
+            {
+                // Free any other managed objects here.
+                //
+                this.host.Close();
+            }
+            // Free any unmanaged objects here.
+            //
+            disposed = true;
+        }
+        #endregion
+    }
+}
