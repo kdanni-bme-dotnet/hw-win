f8615534cec3301a516ad428e7441bf8bd4caef8
Dániel Kiss <kissdani89@gmail.com>
2015-11-13 10:54:10 +0100

    wpf client initialized

---
+++ WPFClient/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace V37ZEN.WPFClient
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
