55d87e732004f03ad3fef7aed44d914afca9b195
Dániel Kiss <kissdani89@gmail.com>
2015-12-13 05:59:29 +0100

    commit

---
+++ DebugGeneral/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DebugGeneral
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ShowNetworkInterfaces();
+
+            System.Console.Read();
+        }
+
+        public static void ShowNetworkInterfaces()
+        {
+            IPGlobalProperties computerProperties = IPGlobalProperties.GetIPGlobalProperties();
+            NetworkInterface[] nics = NetworkInterface.GetAllNetworkInterfaces();
+            Console.WriteLine("Interface information for {0}.{1}     ",
+                    computerProperties.HostName, computerProperties.DomainName);
+            if (nics == null || nics.Length < 1)
+            {
+                Console.WriteLine("  No network interfaces found.");
+                return;
+            }
+
+            string concatenatedMACs = "";
+
+            Console.WriteLine("  Number of interfaces .................... : {0}", nics.Length);
+            foreach (NetworkInterface adapter in nics)
+            {
+                IPInterfaceProperties properties = adapter.GetIPProperties(); //  .GetIPInterfaceProperties();
+                Console.WriteLine();
+                Console.WriteLine(adapter.Description);
+                Console.WriteLine(String.Empty.PadLeft(adapter.Description.Length, '='));
+                Console.WriteLine("  Interface type .......................... : {0}", adapter.NetworkInterfaceType);
+                Console.Write("  Physical address ........................ : ");
+                PhysicalAddress address = adapter.GetPhysicalAddress();
+                concatenatedMACs += address.ToString();
+                byte[] bytes = address.GetAddressBytes();
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    // Display the physical address in hexadecimal.
+                    Console.Write("{0}", bytes[i].ToString("X2"));
+                    // Insert a hyphen after each byte, unless we are at the end of the 
+                    // address.
+                    if (i != bytes.Length - 1)
+                    {
+                        Console.Write("-");
+                    }
+                }
+                Console.WriteLine();
+            }
+
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine(concatenatedMACs);
+
+            // step 1, calculate MD5 hash from input
+            MD5 md5 = System.Security.Cryptography.MD5.Create();
+            byte[] inputBytes = System.Text.Encoding.Unicode.GetBytes(concatenatedMACs);
+            byte[] hash = md5.ComputeHash(inputBytes);
+
+            // step 2, convert byte array to hex string
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < hash.Length; i++)
+            {
+                sb.Append(hash[i].ToString("X2"));
+            }
+            Console.WriteLine(sb);
+        }
+    }
+}
---
+++ Lab_WPF_ControlExample/PersonManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_WPF_ControlExample
+{
+    class PersonManager
+    {
+        private static readonly Lazy<ObservableCollection<Person>> peopleHolder
+            = new Lazy<ObservableCollection<Person>>(
+                () => {
+                    var rnd = new Random();
+                    return new ObservableCollection<Person>
+                    (
+                        Enumerable.Range(0, 5).Select(i =>
+                        new Person
+                        {
+                            Id = Guid.NewGuid(),
+                            Name = "Person " + i,
+                            Age = rnd.Next(20, 100)
+                        }
+                    ));
+                });
+
+        public static ObservableCollection<Person> People
+        {
+            get { return peopleHolder.Value; }
+        }
+    }
+}
