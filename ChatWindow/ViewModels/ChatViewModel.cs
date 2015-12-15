82d985fe275dfb5dc719ec9c469307fd026e5cc5
Kiss Dániel <kissdani89@gmail.com>
2015-12-15 16:16:05 +0100

    partial class

--- ChatWindow/ViewModels/ChatViewModel.cs
+++ ChatWindow/ViewModels/ChatViewModel.cs
@@ -1,20 +1,13 @@
 ﻿using ChatWindow.Logic;
 using ChatWindow.Models;
-using MVVMExample.Commands;
 using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Input;
 
 namespace ChatWindow.ViewModels
 {
-    class ChatViewModel : INotifyPropertyChanged
+    partial class ChatViewModel : INotifyPropertyChanged
     {
 
         public bool IsReady { get; set; }
@@ -24,13 +17,6 @@ namespace ChatWindow.ViewModels
         public ObservableCollection<Chatter> ChatterList { get; set; }
 
         public ObservableCollection<Message> MessageFlow { get; set; }
-
-        private string _text;
-        public string MessageText { get { return _text; } set { _text = value; OnPropertyChanged(); }}
-
-        private GridLength _sendbuttonWidth;
-        public GridLength SendbuttonWidth { get { return _sendbuttonWidth; }
-            set { _sendbuttonWidth = value; OnPropertyChanged(); }}
 
         public readonly Chatter Self = new Chatter { Nick = Chatter.Anonymous, ThisIsMe = true };
 
@@ -52,28 +38,58 @@ namespace ChatWindow.ViewModels
             }
         }
 
-        #region sendCommand
-        private RelayCommand _sendCommand;
-
-        public ICommand SendCommand
+        private void Help()
         {
-            get
+            foreach (Message m in GetHelpMessage())
             {
-                if(_sendCommand == null)
-                {
-                    _sendCommand = new RelayCommand(
-                        l => SendMessage(), l => CanSend());
-                }
-                return _sendCommand;
+                MessageFlow.Add(m);
             }
         }
 
-        private bool CanSend()
+        private void Exit()
         {
-            return IsReady;
+            System.Windows.Application.Current.Shutdown();
         }
-        #endregion
 
+        private void EasyAccess()
+        {
+            if (SendbuttonWidth.Value == 0)
+            {
+                SendbuttonWidth = new GridLength(1, GridUnitType.Star);
+            }
+            else
+            {
+                SendbuttonWidth = new GridLength(0, GridUnitType.Star);
+            }
+        }
+
+        private void HandleChatCommand()
+        {
+            if ("/help".Equals(MessageText))
+            {
+                Help();
+                return;                    
+            }
+            if ("/exit".Equals(MessageText))
+            {
+                Exit();
+                return;
+            }
+            if ("/ea".Equals(MessageText))
+            {
+                EasyAccess();
+                return;
+            }
+            MessageFlow.Add(
+                new Message
+                {
+                    Chatter = Application,
+                    Timestamp = DateTime.UtcNow,
+                    TextMessage = "No " + MessageText + " command found!",
+                    Type = MessageType.Meta
+                });
+        }
+        
         private void SendMessage()
         {
             if (MessageText != null && MessageText.StartsWith("/"))
@@ -93,92 +109,6 @@ namespace ChatWindow.ViewModels
                 this.MessageFlow.Add(m);
             }
             MessageText = "";
-        }
-
-        private void HandleChatCommand()
-        {
-            if ("/help".Equals(MessageText))
-            {
-                foreach (Message m in GetHelpMessage())
-                {
-                    MessageFlow.Add(m);
-                }
-                return;
-                    
-            }
-            if ("/ea".Equals(MessageText))
-            {
-                if(SendbuttonWidth.Value == 0)
-                {
-                    SendbuttonWidth = new GridLength(1, GridUnitType.Star);
-                } else
-                {
-                    SendbuttonWidth = new GridLength(0, GridUnitType.Star);
-                }
-            }
-        }
-
-        #region messages
-        public List<Message> GetWelcomeMessage()
-        {
-            var theList = new List<Message>();
-
-            theList.Add(
-                new Message
-                {
-                    Chatter = Application,
-                    Timestamp = DateTime.UtcNow,
-                    TextMessage = "Welcome " + Self.Nick + "!",
-                    Type = MessageType.Meta
-                });
-
-            theList.Add(
-                new Message
-                {
-                    Chatter = Application,
-                    Timestamp = DateTime.UtcNow,
-                    TextMessage = "Type /help for help.",
-                    Type = MessageType.Meta
-                });
-
-
-            return theList;
-        }
-
-        public List<Message> GetHelpMessage()
-        {
-            var theList = new List<Message>();
-
-            theList.Add(
-                new Message
-                {
-                    Chatter = Application,
-                    Timestamp = DateTime.UtcNow,
-                    TextMessage = "/help\tDisplay this help.",
-                    Type = MessageType.Meta
-                });
-            theList.Add(
-                new Message
-                {
-                    Chatter = Application,
-                    Timestamp = DateTime.UtcNow,
-                    TextMessage = "/ea\tEasy access.",
-                    Type = MessageType.Meta
-                });
-
-            return theList;
-        }
-        #endregion
-
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
-        {
-            var handler = PropertyChanged;
-            if (handler != null)
-            {
-                handler(this, new PropertyChangedEventArgs(propertyName));
-            }
         }
     }
 }
---
+++ ChatWindow/ViewModels/ChatViewModelJunk.cs
@@ -0,0 +1,117 @@
+﻿using ChatWindow.Models;
+using MVVMExample.Commands;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using System.Windows.Input;
+
+namespace ChatWindow.ViewModels
+{
+    partial class ChatViewModel
+    {
+        private string _text;
+        public string MessageText { get { return _text; } set { _text = value; OnPropertyChanged(); } }
+
+        private GridLength _sendbuttonWidth;
+        public GridLength SendbuttonWidth
+        {
+            get { return _sendbuttonWidth; }
+            set { _sendbuttonWidth = value; OnPropertyChanged(); }
+        }
+
+        #region messages
+        public List<Message> GetWelcomeMessage()
+        {
+            var theList = new List<Message>();
+
+            theList.Add(
+                new Message
+                {
+                    Chatter = Application,
+                    Timestamp = DateTime.UtcNow,
+                    TextMessage = "Welcome " + Self.Nick + "!",
+                    Type = MessageType.Meta
+                });
+
+            theList.Add(
+                new Message
+                {
+                    Chatter = Application,
+                    Timestamp = DateTime.UtcNow,
+                    TextMessage = "Type /help for help.",
+                    Type = MessageType.Meta
+                });
+
+
+            return theList;
+        }
+
+        public List<Message> GetHelpMessage()
+        {
+            var theList = new List<Message>();
+
+            theList.Add(
+                new Message
+                {
+                    Chatter = Application,
+                    Timestamp = DateTime.UtcNow,
+                    TextMessage = "/help\tDisplay this help.",
+                    Type = MessageType.Meta
+                });
+            theList.Add(
+                new Message
+                {
+                    Chatter = Application,
+                    Timestamp = DateTime.UtcNow,
+                    TextMessage = "/exit\tClose the application.",
+                    Type = MessageType.Meta
+                });
+            theList.Add(
+                new Message
+                {
+                    Chatter = Application,
+                    Timestamp = DateTime.UtcNow,
+                    TextMessage = "/ea\tEasy access.",
+                    Type = MessageType.Meta
+                });
+
+            return theList;
+        }
+        #endregion
+        
+        #region sendCommand
+        private RelayCommand _sendCommand;
+
+        public ICommand SendCommand
+        {
+            get
+            {
+                if (_sendCommand == null)
+                {
+                    _sendCommand = new RelayCommand(
+                        l => SendMessage(), l => CanSend());
+                }
+                return _sendCommand;
+            }
+        }
+
+        private bool CanSend()
+        {
+            return IsReady;
+        }
+        #endregion
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            var handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
